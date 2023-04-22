c24ac257875d8fa233647132f7b2fc36c2cec72c
Feifei Huang <n00221814@iadt.ie>
2023-04-22 15:15:18 +0100

    Update article php

--- 00 -  Starter Files/00 -  Starter Files/article.php
+++ 00 -  Starter Files/00 -  Starter Files/article.php
@@ -1,4 +1,15 @@
-
+<?php
 $mainStory = Story::findById($_GET["id"]);
-
+     foreach ($topStory as $story) { ?>
+         <div class="content">
+             <div class="image">
+                 <img src= "<?= $story->images ?>">
+             </div>
+             <a href="article.php?id=<?= $story->id?> "> <h1><?=$story->heading ?></h1> </a>
+             <p>
+              <?= substr($story->article,0,200); ?>
+             </p>
+             <h5>BY <?=$story->author ?></h5>
+         </div>
 <h3><?=$mainStory->article ?></h3>
+<?php } ?>
